51865aad31eb889297aeaef9c36fd81fc59ae5ff
Chayon <chayondhar08@gmail.com>
2025-11-11 12:27:23 +0600

    param fix v1

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,4 +1,3 @@
-// app/listings/[listingId]/page.tsx
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import getReservations from "@/app/actions/getReservations";
@@ -6,13 +5,24 @@ import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
 
+
 interface IParams {
   listingId?: string;
 }
 
-const ListingPage = async ({ params }: { params: IParams }) => {
-  const listing = await getListingById(params);
-  const reservations = await getReservations(params);
+
+interface PageProps {
+  params: Promise<IParams>; 
+}
+
+
+const ListingPage = async ({ params }: PageProps) => {
+
+  const resolvedParams = await params;
+
+
+  const listing = await getListingById(resolvedParams);
+  const reservations = await getReservations(resolvedParams);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
